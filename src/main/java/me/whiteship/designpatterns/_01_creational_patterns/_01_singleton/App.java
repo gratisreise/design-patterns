f025f353568dc621deee05bc4c82f41b73e7552a
noah <jeonghogim318@gmail.com>
2025-02-04 13:00:00 +0900

    singleton

---
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -0,0 +1,28 @@
+package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+public class App {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Settings5 settings = Settings5.INSTANCE;
+
+        Settings5 settings1 = null;
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings);
+        }
+
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            settings1 = (Settings5) in.readObject();
+        }
+
+        System.out.println(settings == settings1);
+    }
+
+}
