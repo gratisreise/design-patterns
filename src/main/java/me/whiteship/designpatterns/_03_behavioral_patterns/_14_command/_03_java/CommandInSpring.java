1ea56c84bcd721012d62ff694af5e63e1d956f10
noah <jeonghogim318@gmail.com>
2025-02-07 14:00:00 +0900

    command

---
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_14_command/_03_java/CommandInSpring.java
@@ -0,0 +1,29 @@
+package me.whiteship.designpatterns._03_behavioral_patterns._14_command._03_java;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import me.whiteship.designpatterns._03_behavioral_patterns._14_command._02_after.Command;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+public class CommandInSpring {
+
+    private DataSource dataSource;
+
+    public CommandInSpring(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void add(Command command) {
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(dataSource)
+                .withTableName("command")
+                .usingGeneratedKeyColumns("id");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", command.getClass().getSimpleName());
+        data.put("when", LocalDateTime.now());
+        insert.execute(data);
+    }
+
+}
