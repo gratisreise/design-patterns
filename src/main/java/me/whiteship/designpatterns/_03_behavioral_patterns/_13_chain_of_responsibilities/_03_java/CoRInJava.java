49e6e6f2760b90a7660c661e0ca961d1fbb8b836
noah <jeonghogim318@gmail.com>
2025-02-07 13:00:00 +0900

    chain of responsibility

---
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_13_chain_of_responsibilities/_03_java/CoRInJava.java
@@ -0,0 +1,22 @@
+package me.whiteship.designpatterns._03_behavioral_patterns._13_chain_of_responsibilities._03_java;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class CoRInJava {
+
+    public static void main(String[] args) {
+        Filter filter = new Filter() {
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                // TODO 전처리
+                chain.doFilter(request, response);
+                // TODO 후처리
+            }
+        };
+    }
+}
